ec1a6b2b57a25e5b907431d307a3240a3a261969
Ritvik Babre <114216082+Ritvikyt@users.noreply.github.com>
2023-11-01 21:43:27 +0530

    movement_clipping_gravity_bugfix
    fixed the camera clipping, gravity and some other bugs

--- Assets/Codes/Player_Movement.cs
+++ Assets/Codes/Player_Movement.cs
@@ -8,12 +8,28 @@ public class Player_Movement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 5f;
+    private float velocity_;
+    private float gravity_ = -9.81f;
+    public float GravityMultiplier = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
 
+    }
+
+    private void ApplyGravity()
+    {
+        if (controller.isGrounded)
+        {
+            velocity_ = -1f;
+        }
+        else 
+        {
+            velocity_ += gravity_ * GravityMultiplier * Time.deltaTime;
+        }
+       
     }
 
     // Update is called once per frame
