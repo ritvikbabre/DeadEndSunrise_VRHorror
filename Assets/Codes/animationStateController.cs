f85f53e1db352175a072fa40c3eca1ce4422d1b6
Hitesh Behera <bhiteshme74@gmail.com>
2023-11-21 01:27:51 +0530

    finish: Running Animation State of Character [Josh]

--- Assets/Codes/animationStateController.cs
+++ Assets/Codes/animationStateController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class animationStateController : MonoBehaviour
 {
     Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,10 @@ public class animationStateController : MonoBehaviour
     void Update()
     {
         bool isWalking = animator.GetBool("isWalking");
+        bool isrunning = animator.GetBool("isRunning");
         bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
         if (!isWalking && forwardPressed)
         {
             animator.SetBool("isWalking", true);
@@ -24,6 +28,18 @@ public class animationStateController : MonoBehaviour
         if (isWalking && !forwardPressed)
         {
             animator.SetBool("isWalking", false);
+        }
+
+        // if player is walking and not running and presses left shift
+        if (!isrunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+        // if player is running and stops running or stops walking
+        if (isrunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool("isRunning", false);
         }
     }
 }
