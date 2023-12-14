1d9e920306685a0032e5bd27fc75b93de6d810ce
Hitesh Behera <bhiteshme74@gmail.com>
2023-12-14 23:28:27 +0530

    new: Crouch Speed

--- Assets/Codes/Player_Movement.cs
+++ Assets/Codes/Player_Movement.cs
@@ -8,15 +8,19 @@ public class Player_Movement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 5f;
+    public float crouchSpeed = 2.5f; // Added crouchSpeed
+    public float crawlSpeed = 1.5f; // Added crawlSpeed
     private float velocity_;
     private float gravity_ = -9.81f;
     public float GravityMultiplier = 3.0f;
+
+    private PlayerAnimationStateController animationStateController;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-
+        animationStateController = GetComponent<PlayerAnimationStateController>(); // Get the PlayerAnimationStateController component
     }
 
     private void ApplyGravity()
@@ -39,6 +43,20 @@ public class Player_Movement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+
+        // Check the animation state and adjust the speed accordingly
+        if (animationStateController.animator.GetBool(animationStateController.isCrouchingHash))
+        {
+            speed = crouchSpeed;
+        }
+        else if (animationStateController.animator.GetBool(animationStateController.isCrawlingHash))
+        {
+            speed = crawlSpeed;
+        }
+        else
+        {
+            speed = 5f;
+        }
 
         controller.Move(move*speed*Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.LeftShift))
