4ef6caac05f17b4adf47e003d9ade2f201167f48
Hitesh Behera <bhiteshme74@gmail.com>
2024-01-08 00:13:02 +0530

    feature: Patrol AI State

--- Assets/Codes/GhoulController.cs
+++ Assets/Codes/GhoulController.cs
@@ -5,16 +5,37 @@ using UnityEngine.AI;
 
 public class GhoulController : MonoBehaviour
 {
+    public enum AISTATE { PATROL, CHASE, ATTACK };
+
     public Transform player;
     public NavMeshAgent enemy;
+    public AISTATE enemyState = AISTATE.PATROL;
+    float distanceOffset = 2.0f;
 
-    public void Update()
+    public List<Transform> waypoints = new List<Transform>();
+    Transform currentWaypoint;
+
+    public void Start()
     {
-        enemy.SetDestination(player.position);
-
-
-
+        currentWaypoint = waypoints[Random.Range(0, waypoints.Count)];
+        StartCoroutine(PatrolState());
     }
+    public IEnumerator PatrolState()
+    { 
+        
+        while (enemyState == AISTATE.PATROL)
+        {
+            enemy.SetDestination(currentWaypoint.position);
+            if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceOffset)
+            {
+                currentWaypoint = waypoints[Random.Range(0, waypoints.Count)];
+                enemy.SetDestination(currentWaypoint.position);
+            }
+            yield return null;
+        }
+    }
+
+
 
 
 
