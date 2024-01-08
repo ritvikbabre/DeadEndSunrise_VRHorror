925a28982e649253d6c10b39859de5765d502759
Hitesh Behera <bhiteshme74@gmail.com>
2024-01-08 23:52:51 +0530

    File Documentization

--- Assets/Codes/GhoulMovementController.cs
+++ Assets/Codes/GhoulMovementController.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class GhoulController : MonoBehaviour
 {
+    
     public enum AISTATE { PATROL, CHASE, ATTACK };
 
     public Transform player;
     public NavMeshAgent enemy;
-    public AISTATE enemyState = AISTATE.PATROL;
-    float distanceOffset = 2.0f;
-    float chaseDistance = 10.0f; // Add a chase distance
+    public AISTATE enemyState = AISTATE.PATROL; // Current state of the Ghoul
+    float distanceOffset = 2.0f; // Distance offset for various calculations
+    float chaseDistance = 10.0f; // Distance at which the Ghoul will start chasing the player
 
     public List<Transform> waypoints = new List<Transform>();
-    Transform currentWaypoint;
+    Transform currentWaypoint; // Current waypoint the Ghoul is moving towards
 
     public void Start()
     {
@@ -24,9 +26,11 @@ public class GhoulController : MonoBehaviour
 
     public void ChangeState(AISTATE newState)
     {
+        // Stop all coroutines to ensure a clean state change
         StopAllCoroutines();
         enemyState = newState;
 
+        // Start the coroutine corresponding to the new state
         switch (enemyState)
         {
             case AISTATE.PATROL:
@@ -43,15 +47,17 @@ public class GhoulController : MonoBehaviour
 
     public IEnumerator ChaseState()
     {
+        // Set the destination of the Ghoul to the player's position
         enemy.SetDestination(player.position);
         while (enemyState == AISTATE.CHASE)
         {
+            // If the player is within the distance offset, switch to Attack state
             if (Vector3.Distance(transform.position, player.position) < distanceOffset)
             {
                 ChangeState(AISTATE.ATTACK);
                 yield break;
             }
-            // If player outruns enemy, go back to patrol state
+            // If the player is too far away, switch back to Patrol state
             if (Vector3.Distance(transform.position, player.position) > chaseDistance)
             {
                 ChangeState(AISTATE.PATROL);
@@ -61,10 +67,12 @@ public class GhoulController : MonoBehaviour
         }
     }
 
+    // Coroutine for the Attack state
     public IEnumerator AttackState()
     {
         while (enemyState == AISTATE.ATTACK)
         {
+            // If the player moves out of the distance offset, switch back to Chase state
             if (Vector3.Distance(transform.position, player.position) > distanceOffset)
             {
                 ChangeState(AISTATE.CHASE);
@@ -74,11 +82,14 @@ public class GhoulController : MonoBehaviour
         }
     }
 
+    // Coroutine for the Patrol state
     public IEnumerator PatrolState()
     {
         while (enemyState == AISTATE.PATROL)
         {
+            // Set the destination of the Ghoul to the current waypoint's position
             enemy.SetDestination(currentWaypoint.position);
+            // If the Ghoul reaches the current waypoint, select a new random waypoint
             if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceOffset)
             {
                 currentWaypoint = waypoints[Random.Range(0, waypoints.Count)];
