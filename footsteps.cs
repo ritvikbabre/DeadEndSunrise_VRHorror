aab0457a3ad5c7bd3c63e014fc6a09e5419e38c2
ShrutiSabbanii <140100022+ShrutiSabbanii@users.noreply.github.com>
2024-02-12 00:34:29 +0530

    ADDED FOOTSTEPS SOUND

---
+++ footsteps.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Engine;
+
+
+public class Footsteps : MonoBehaviour
+{
+	public AudioSource FootstepsSound;
+		void Update()
+	{
+		if (Input.Getkey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D){
+			FootstepsSound.enabled = true;
+		}
+		else
+		{ FootstepsSound.enabled = false;
+		}
+			
+
+	}
+}
